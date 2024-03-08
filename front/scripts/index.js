be00fd314b2076f4600015684eb5be23ef7e8501
JSfranco <lombofrancojuansebastian@gmail.com>
2024-03-08 00:21:33 -0300

    crecion de la funcion createPreview

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,31 +2,39 @@ const imgContainer = document.querySelector(".img-container");
 
 function createImgDisplay(data) {
   data.forEach((item, index) => {
-    const info = document.createElement("div");
-    info.classList.add("movie-data");
-    info.innerHTML = `
-         <h3>${item.title}</h3>
-         <p>Year: ${item.year}</p>
+    /*
          <p>Director: ${item.director}</p>
          <p>Duration: ${item.duration}</p>
          <p>Genre: ${item.genre.join(", ")}</p>
          <p>Rate: ${item.rate}</p>
         `;
-
+        */
 
     const imgDisplay = document.createElement("div"); // creamos el elemento div que almacenara las peliculas
     imgDisplay.classList.add("card-body"); // asignamos la clase al nuevo div
 
     imgDisplay.innerHTML = `
-        <img  class="card-img-top" src="${item.poster}" alt="${item.title}">
+<a href="./preview.html"> <img  class="card-img-top" src="${item.poster}" alt="${item.title}" onclick="createPreview()"></a>
+       
         
       `;
 
-
     imgContainer.appendChild(imgDisplay); //insertamos en el div padre
-    imgDisplay.appendChild(info)
-    
+    //imgDisplay.appendChild(info)
   });
+}
+
+function createPreview() {
+  
+    const preview = document.createElement("div"); //creamos un div contenedor para el  preview
+    preview.classList.add("container");
+
+    preview.innerHTML = `
+    <h1>--mmu--oHELLO THEREo--umm--</h1>
+
+    `;
+
+    previewContainer.appendChild(preview); //este es el nuevo contenedor donde veremos los datos de la pelicula 
 }
 
 $(document).ready(function () {
